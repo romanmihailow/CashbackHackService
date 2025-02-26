a4921525d3f23883795e371ff8c134c09ab02f3b
romanmihailow <romanmihailow@gmail.com>
2025-02-26 11:24:59 +0500

    juni4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;   ///Asserts расположены в классе org.testng.Assert. Обратите внимание: в TestNG принято actual, expected вместо expected, actual
-import org.testng.annotations.Test; ///Аннотация @Test должна иметь Fully Qualified Name org.testng.annotations.Tes
+import org.junit.Assert;   ///Asserts расположены в классе org.testng.Assert. Обратите внимание: в TestNG принято actual, expected вместо expected, actual
+import org.junit.Test; ///Аннотация @Test должна иметь Fully Qualified Name org.testng.annotations.Tes
 
 public class CashbackHackServiceTest {
 
@@ -9,20 +9,20 @@ public class CashbackHackServiceTest {
     public void testCashbackHackService900() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(900);
-        Assert.assertEquals(result, 100, "Ожидаемый остаток для 900: 100");
+        Assert.assertEquals(100, result);
     }
 
     @Test
     public void testCashbackHackService1000() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(1000);
-        Assert.assertEquals(result, 1000, "Ожидаемый остаток для 1000: 1000");
+        Assert.assertEquals(0, result); ///ОШИБКА при покупке на 1000 не должно выводиться купите еще на 1000
     }
     @Test
     public void testCashbackHackService1100() {
         CashbackHackService service = new CashbackHackService();
         int result = service.remain(1100);
-        Assert.assertEquals(result, 900, "Ожидаемый остаток для 1100: 900");
+        Assert.assertEquals(900, result);
     }
 
 
